7407cfc48bc8a37a2ffc733df1394e5f1c0fcfac
Benton Drew <benton.s.drew@drewantech.com>
2017-03-18 09:22:02 -0700

    Implement web server interface as a web api.

--- service/demo_matrix_multiplication_web_service.py
+++ service/demo_matrix_multiplication_web_service.py
@@ -3,50 +3,56 @@
 #  ALL RIGHTS RESERVED
 
 from flask import (Flask,
-                   render_template,
-                   flash,
-                   redirect,
-                   url_for)
+                   jsonify)
 from os import urandom
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
 
-@app.route('/', methods=['GET'])
-def index():
-  return render_template('index.html')
-
-
-@app.route('/database_initialization')
-def database_initialization():
+@app.route('/create_matrix_database', methods=['PUT'])
+def create_matrix_database():
   try:
     from demo_matrix_multiplication.database_initialization \
-        import (create_database,
-                create_tables)
-    flash('Attempting to create matrix database.')
+        import (create_database)
     create_database()
-    flash('Successfully created matrix database.')
-    flash('Attempting to create matrix database tables.')
-    create_tables()
-    flash('Successfully created matrix database tables.')
+    return jsonify({'status': 'Successfully created matrix database.'}), 201
   except Exception as e:
-    raise flash('Error in initializing databases: {}'.format(e))
-  return redirect(url_for('index'))
+    return jsonify({'status': 'Error in creating matrix database: {}'
+                   .format(e)}), 500
 
 
-@app.route('/database_tear_down')
-def database_tear_down():
+@app.route('/create_matrix_tables', methods=['PUT'])
+def create_matrix_tables():
+  try:
+    from demo_matrix_multiplication.database_initialization \
+        import (create_tables)
+    create_tables()
+    return jsonify({'status': 'Successfully created matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix tables: {}'
+                   .format(e)}), 500
+
+
+@app.route('/drop_matrix_database', methods=['DELETE'])
+def drop_matrix_database():
   try:
     from demo_matrix_multiplication.database_tear_down \
-        import (drop_database,
-                drop_tables)
-    flash('Attempting to drop matrix database tables.')
-    drop_tables()
-    flash('Successfully dropped matrix database tables.')
-    flash('Attempting to drop matrix database.')
+        import (drop_database)
     drop_database()
-    flash('Successfully dropped matrix database.')
+    return jsonify({'status': 'Successfully dropped matrix database.'}), 201
   except Exception as e:
-    raise flash('Error in tearing down databases: {}'.format(e))
-  return redirect(url_for('index'))
+    return jsonify({'status': 'Error in dropping matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('/drop_martix_tables', methods=['DELETE'])
+def drop_martix_tables():
+  try:
+    from demo_matrix_multiplication.database_tear_down \
+        import (drop_tables)
+    drop_tables()
+    return jsonify({'status': 'Successfully dropped matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix tables: {}'
+                   .format(e)}), 500
